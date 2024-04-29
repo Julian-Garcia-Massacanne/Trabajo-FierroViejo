96246d902bb0fa394634825a60c65eb4f85d0a0c
Lucio Colli <luciocolli1@gmail.com>
2024-04-29 16:14:56 -0300

    Agregando rutas

---
+++ routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+/*import {joinCarrera, joinCarreraExistente, play, poll } from './controllers/controladorFinal.js';
+
+var routerServer = express.Router();
+routerServer.post('/carrera/board', joinCarrera); //  ingresa jugador 1
+routerServer.patch('/carrera/board/:boardId', joinCarreraExistente);  // ingresa jugador 2    
+routerServer.post('/carrera/board/:boardId', play);
+routerServer.get('/carrera/board/:boardId', poll);
+*/
+
+
+
+export default routerServer;
