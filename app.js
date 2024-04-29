ec5ccddf178462bac2bcf4045177969959999a8b
Julian-Garcia <julian020404@gmail.com>
2024-04-29 16:04:29 -0300

    Toqueteando

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+
+
+const app = express();
+const PORT = 8000
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>{
+    console.log(`Server running in htpp://localhost:8000`);
+});
+
+app.get('/', (req,res) => {
+    res.send("H");
+})
